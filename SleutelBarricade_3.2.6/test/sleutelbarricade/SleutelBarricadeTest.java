db52b779b03133bd97fec9af000460a4688a02ec
tom <tspek9@gmail.com>
2018-04-06 03:38:25 +0200

    expanded unit tests

--- SleutelBarricade_3.2.6/test/sleutelbarricade/SleutelBarricadeTest.java
+++ SleutelBarricade_3.2.6/test/sleutelbarricade/SleutelBarricadeTest.java
@@ -43,7 +43,47 @@ public class SleutelBarricadeTest {
     }
     
     @Test
-    public void moveToFilledLocationCorrect(){
+    public void moveUpToFilledLocationCorrect(){
+        GameObject[][] playField = new GameObject[10][10];
+        boolean[][] filledSpots = new boolean[10][10];
+        Player p = new Player("Player", 37, 87, sb);
+        playField[1][1] = p;
+        filledSpots[1][1] = true;
+        
+        Wall w = new Wall("Wall", 37, 37);
+        playField[1][0] = w;
+        filledSpots[1][0] = true;
+        
+        lijst.add(p);
+        lijst.add(w);
+        
+        boolean expected = false;
+        boolean result = p.movePossible(lijst, playField, filledSpots, "UP");
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void moveUpToFilledLocationIncorrect(){
+        GameObject[][] playField = new GameObject[10][10];
+        boolean[][] filledSpots = new boolean[10][10];
+        Player p = new Player("Player", 37, 37, sb);
+        playField[1][1] = p;
+        filledSpots[1][1] = true;
+        
+        Wall w = new Wall("Wall", 37, 87);
+        playField[1][0] = w;
+        filledSpots[1][0] = true;
+        
+        lijst.add(p);
+        lijst.add(w);
+        
+        boolean expected = true;
+        boolean result = p.movePossible(lijst, playField, filledSpots, "RIGHT");
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void moveDownToFilledLocationCorrect(){
         GameObject[][] playField = new GameObject[10][10];
         boolean[][] filledSpots = new boolean[10][10];
         Player p = new Player("Player", 37, 37, sb);
@@ -51,6 +91,86 @@ public class SleutelBarricadeTest {
         filledSpots[0][0] = true;
         
         Wall w = new Wall("Wall", 37, 87);
+        playField[0][1] = w;
+        filledSpots[0][1] = true;
+        
+        lijst.add(p);
+        lijst.add(w);
+        
+        boolean expected = false;
+        boolean result = p.movePossible(lijst, playField, filledSpots, "RIGHT");
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void moveDownToFilledLocationIncorrect(){
+        GameObject[][] playField = new GameObject[10][10];
+        boolean[][] filledSpots = new boolean[10][10];
+        Player p = new Player("Player", 37, 37, sb);
+        playField[0][0] = p;
+        filledSpots[0][0] = true;
+        
+        Wall w = new Wall("Wall", 37, 87);
+        playField[0][1] = w;
+        filledSpots[0][1] = true;
+        
+        lijst.add(p);
+        lijst.add(w);
+        
+        boolean expected = true;
+        boolean result = p.movePossible(lijst, playField, filledSpots, "DOWN");
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void moveLeftToFilledLocationCorrect(){
+        GameObject[][] playField = new GameObject[10][10];
+        boolean[][] filledSpots = new boolean[10][10];
+        Player p = new Player("Player", 137, 37, sb);
+        playField[1][0] = p;
+        filledSpots[1][0] = true;
+        
+        Wall w = new Wall("Wall", 87, 37);
+        playField[0][0] = w;
+        filledSpots[0][0] = true;
+        
+        lijst.add(p);
+        lijst.add(w);
+        
+        boolean expected = false;
+        boolean result = p.movePossible(lijst, playField, filledSpots, "LEFT");
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void moveLeftToFilledLocationIncorrect(){
+        GameObject[][] playField = new GameObject[10][10];
+        boolean[][] filledSpots = new boolean[10][10];
+        Player p = new Player("Player", 137, 37, sb);
+        playField[3][0] = p;
+        filledSpots[3][0] = true;
+        
+        Wall w = new Wall("Wall", 87, 37);
+        playField[2][0] = w;
+        filledSpots[2][0] = true;
+        
+        lijst.add(p);
+        lijst.add(w);
+        
+        boolean expected = true;
+        boolean result = p.movePossible(lijst, playField, filledSpots, "LEFT");
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void moveRightToFilledLocationCorrect(){
+        GameObject[][] playField = new GameObject[10][10];
+        boolean[][] filledSpots = new boolean[10][10];
+        Player p = new Player("Player", 37, 37, sb);
+        playField[0][0] = p;
+        filledSpots[0][0] = true;
+        
+        Wall w = new Wall("Wall", 87, 37);
         playField[1][0] = w;
         filledSpots[1][0] = true;
         
@@ -63,14 +183,14 @@ public class SleutelBarricadeTest {
     }
     
     @Test
-    public void moveToFilledLocationIncorrect(){
+    public void moveRightToFilledLocationIncorrect(){
         GameObject[][] playField = new GameObject[10][10];
         boolean[][] filledSpots = new boolean[10][10];
         Player p = new Player("Player", 37, 37, sb);
         playField[0][0] = p;
         filledSpots[0][0] = true;
         
-        Wall w = new Wall("Wall", 37, 87);
+        Wall w = new Wall("Wall", 87, 37);
         playField[1][0] = w;
         filledSpots[1][0] = true;
         
